a317bc8921514432ea42fa6bbaf75c4cc4095d0f
Johann Bogota <johann.bogota@mail.escuelaing.edu.co>
2021-10-25 19:48:33 -0500

    corrections and add router dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NotFound from "./pages/not_found/NotFound"
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <BrowserRouter>
+      <Switch>
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
