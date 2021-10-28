ac5c67cba27837b60fdfe35e57572c951f9bc044
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2021-10-27 20:09:07 -0500

    Added Image and details

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import NotFound from "./pages/not_found/NotFound"
-import { ChakraProvider } from "@chakra-ui/react"
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import NotFound from './pages/not_found/NotFound';
+import { Product } from './services/product/Product';
 
 ReactDOM.render(
-  <ChakraProvider>
-    <React.StrictMode>
-      <BrowserRouter>
-        <Switch>
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </React.StrictMode>
-  </ChakraProvider>,
-  document.getElementById('root')
+	<ChakraProvider>
+		<React.StrictMode>
+			<BrowserRouter>
+				<Switch>
+					<Route path="/product">
+						<Product />
+					</Route>
+					<Route component={NotFound} />
+				</Switch>
+			</BrowserRouter>
+		</React.StrictMode>
+	</ChakraProvider>,
+	document.getElementById('root')
 );
