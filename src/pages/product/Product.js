6707cec6303f54817d427718d129b94143fc608e
Cesar Ortiz <cesar.ortiz@mail.escuelaing.edu.co>
2021-11-04 11:43:09 -0500

    actualizando vista producto

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -15,7 +15,12 @@ import {
 	NumberDecrementStepper,
 	NumberInputStepper,
 	NumberInputField,
-	NumberInput
+	NumberInput,
+	Tabs, 
+	TabList, 
+	TabPanels, 
+	Tab, 
+	TabPanel
 } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
 
@@ -43,126 +48,141 @@ export const Product = () => {
 	};
 
 	return (
-		<Container maxW={'8xl'}>
-			<HStack justify={'right'} py={6}>
-				<Button leftIcon={<CircleIcon boxSize={7} color="red.500" />} colorScheme="teal" variant="solid">
-					{itemsCounter}
-				</Button>
-			</HStack>
-			<Stack
-				align={'center'}
-				spacing={{ base: 8, md: 20 }}
-				py={{ base: 0, md: 28 }}
-				direction={{ base: 'column', md: 'row' }}
-			>
-				<Stack flex={1} spacing={{ base: 5, md: 10 }}>
-					<Blob
-						w={'80%'}
-						h={'55%'}
-						position={'absolute'}
-						top={{ base: '6%', lg: '28%' }}
-						left={{ base: 2, md: -300, lg: -380 }}
-						zIndex={-1}
-						color={useColorModeValue('green.300', 'green.200')}
-					/>
-					<Box
-						position={'relative'}
-						height={'380px'}
-						rounded={'2xl'}
-						boxShadow={'2xl'}
-						width={'full'}
-						overflow={'hidden'}
-					>
-						<Image
-							alt={'Product Image'}
-							fit={'cover'}
+		<Tabs isFitted variant="enclosed">
+			<TabList>
+				<Tab _selected={{ color: "white", bg: "green.300" }}> Productos</Tab>
+				<Tab _selected={{ color: "white", bg: "green.300" }}>Manuel de uso</Tab>
+			</TabList>
+			<TabPanels>
+    			<TabPanel>
+					<Container maxW={'8xl'}>
+						<HStack justify={'right'} py={6}>
+							<Button leftIcon={<CircleIcon boxSize={7} color="red.500" />} colorScheme="teal" variant="solid">
+								{itemsCounter}
+							</Button>
+						</HStack>
+						<Stack
 							align={'center'}
-							w={'full'}
-							h={'100%'}
-							src={
-								'https://www.gardentech.com/-/media/Images/GardenTech-NA/US/products/Daconil-family-in-use.jpg'
-							}
-						/>
-					</Box>
-				</Stack>
-				<Stack flex={1} spacing={{ base: 7, md: 10 }}>
-					<Heading lineHeight={1.1} fontWeight={600} fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
-						<Text
-							as={'span'}
-							position={'relative'}
-							_after={{
-								content: "''",
-								width: 'full',
-								height: '25%',
-								position: 'absolute',
-								bottom: 1,
-								left: 0,
-								bg: 'green.400',
-								zIndex: -1
-							}}
+							spacing={{ base: 8, md: 20 }}
+							py={{ base: 0, md: 28 }}
+							direction={{ base: 'column', md: 'row' }}
 						>
-							{plantName}
-						</Text>
-						<br />
-						<Text as={'span'} color={'green.400'} fontSize={'70%'}>
-							{plantCode}
-						</Text>
-						<br />
-						<Text as={'span'} fontSize={'50%'}>
-							{plantPrice}
-						</Text>
-					</Heading>
-					<Stack spacing={{ base: 4, sm: 6 }} direction={'row'}>
-						<NumberInput size="lg" maxW={32} defaultValue={1} min={1} max={10}>
-							<NumberInputField backgroundColor={'white'} />
-							<NumberInputStepper>
-								<NumberIncrementStepper />
-								<NumberDecrementStepper />
-							</NumberInputStepper>
-						</NumberInput>
-						<Button
-							rounded={'full'}
-							size={'lg'}
-							fontWeight={'normal'}
-							px={6}
-							color={'white'}
-							colorScheme="red"
-							leftIcon={<AddIcon h={4} w={4} color={'white.300'} onSubmit={handleSubmit} />}
-						>
-							Add to Cart
-						</Button>
-					</Stack>
-					<Text fontSize={'130%'}>
-						Con el fungicida Daconil® listo para usar, usted puede prevenir las enfermedades causadas por
-						hongos antes de que broten, o combatirlas para detener y controlar las enfermedades activas. La
-						botella con rociador lista para llevar simplifica el tratamiento a sus plantas de flores,
-						vegetales, frutales, arbustos y árboles, con una protección resistente a la lluvia en la que
-						puede confiar.
-					</Text>
-					
-				</Stack>
-			</Stack>
-			<HStack>
-				<Heading lineHeight={1.1} fontWeight={500} fontSize={{ base: '3xl', sm: '4xl', lg: '5xl' }}>
-					<Text
-						as={'span'}
-						position={'relative'}
-						_after={{
-							content: "''",
-							width: 'full',
-							height: '15%',
-							position: 'absolute',
-							bottom: 2,
-							left: 0,
-							bg: 'red.400',
-							zIndex: -1
-						}}
-					>
-						También te puede interesar
-					</Text>
-				</Heading>
-			</HStack>
-		</Container>
+							<Stack flex={1} spacing={{ base: 5, md: 10 }}>
+								<Blob
+									w={'80%'}
+									h={'55%'}
+									position={'absolute'}
+									top={{ base: '6%', lg: '28%' }}
+									left={{ base: 2, md: -300, lg: -380 }}
+									zIndex={-1}
+									color={useColorModeValue('green.300', 'green.200')}
+								/>
+								<Box
+									position={'relative'}
+									maxH="380px"
+									height={'200px'}
+									rounded={'2xl'}
+									boxShadow={'2xl'}
+									width={'full'}
+									overflow={'hidden'}
+
+								>
+									<Image
+										alt={'Product Image'}
+										fit={'cover'}
+										align={'center'}
+										w={'full'}
+										h={'100%'}
+										src={
+											'https://www.gardentech.com/-/media/Images/GardenTech-NA/US/products/Daconil-family-in-use.jpg'
+										}
+									/>
+								</Box>
+							</Stack>
+							<Stack flex={1} spacing={{ base: 7, md: 10 }}>
+								<Heading lineHeight={1.1} fontWeight={600} fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
+									<Text
+										as={'span'}
+										position={'relative'}
+										_after={{
+											content: "''",
+											width: 'full',
+											height: '25%',
+											position: 'absolute',
+											bottom: 1,
+											left: 0,
+											bg: 'green.400',
+											zIndex: -1
+										}}
+									>
+										{plantName}
+									</Text>
+									<br />
+									<Text as={'span'} color={'green.400'} fontSize={'70%'}>
+										{plantCode}
+									</Text>
+									<br />
+									<Text as={'span'} fontSize={'50%'}>
+										{plantPrice}
+									</Text>
+								</Heading>
+								<Stack spacing={{ base: 4, sm: 6 }} direction={'row'}>
+									<NumberInput size="lg" maxW={32} defaultValue={1} min={1} max={10}>
+										<NumberInputField backgroundColor={'white'} />
+										<NumberInputStepper>
+											<NumberIncrementStepper />
+											<NumberDecrementStepper />
+										</NumberInputStepper>
+									</NumberInput>
+									<Button
+										rounded={'full'}
+										size={'lg'}
+										fontWeight={'normal'}
+										px={6}
+										color={'white'}
+										colorScheme="red"
+										leftIcon={<AddIcon h={4} w={4} color={'white.300'} onSubmit={handleSubmit} />}
+									>
+										Add to Cart
+									</Button>
+								</Stack>
+								<Text fontSize={'100%'}>
+									Con el fungicida Daconil® listo para usar, usted puede prevenir las enfermedades causadas por
+									hongos antes de que broten, o combatirlas para detener y controlar las enfermedades activas. La
+									botella con rociador lista para llevar simplifica el tratamiento a sus plantas de flores,
+									vegetales, frutales, arbustos y árboles, con una protección resistente a la lluvia en la que
+									puede confiar.
+								</Text>
+								
+							</Stack>
+						</Stack>
+						<HStack>
+							<Heading lineHeight={1.1} fontWeight={500} fontSize={{ base: '3xl', sm: '4xl', lg: '5xl' }}>
+								<Text
+									as={'span'}
+									position={'relative'}
+									_after={{
+										content: "''",
+										width: 'full',
+										height: '15%',
+										position: 'absolute',
+										bottom: 2,
+										left: 0,
+										bg: 'red.400',
+										zIndex: -1
+									}}
+								>
+									También te puede interesar
+								</Text>
+							</Heading>
+						</HStack>
+					</Container>
+				</TabPanel>
+				<TabPanel>
+					hola
+				</TabPanel>
+			</TabPanels>
+		</Tabs>
 	);
 };
 
