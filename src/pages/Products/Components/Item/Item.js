a7b3b516a5349eb8ed9458946e73de9512df60e5
johan guerrero <johannes26@192.168.0.11>
2021-11-04 03:13:50 -0500

    arreglo vista productos

--- src/pages/Products/Components/Item/Item.js
+++ src/pages/Products/Components/Item/Item.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, AccordionPanel, Checkbox, Flex, Image, Text, IconButton, Divider } from '@chakra-ui/react'
+import { Box, AccordionPanel, Checkbox, Flex, Image, Text, IconButton, Divider, Center } from '@chakra-ui/react'
 import { AddIcon } from '@chakra-ui/icons'
 const Item = ({ isChecked, onChange, id, name, url, description, price }) => {
 
@@ -12,24 +12,28 @@ const Item = ({ isChecked, onChange, id, name, url, description, price }) => {
     };
 
     return (
-        <AccordionPanel ml="4" mr="4" mb="5" style={ColorComponent}  borderRadius="20px" boxShadow="base">
+        <AccordionPanel ml="4" mr="4" mb="5" style={ColorComponent} borderRadius="20px" boxShadow="base">
             <Divider mb="5" />
             <Checkbox isChecked={isChecked} onChange={onChange}
                 w="100%">
-                <Flex alignItems="center">
-                    <Image src={url} boxSize="90px" objectFit="scale-down" />
-                    <Box ml="3">
-                        <Text textTransform="uppercase" fontWeight="black" fontSize="18">
-                            {name}
-                        </Text>
-                        <Text ml="3" fontSize="sm">{description}</Text>
-                        <Text ml="3" fontSize="m" fontWeight="semibold">$ {price}</Text>
+                <Box p={3} display={{ md: "flex" }}>
+                    <Box flexShrink={0} >
+                        <Center>
+                            <Image src={url} boxSize="90px" objectFit="scale-down" />
+                        </Center>
                     </Box>
-                    <IconButton ml="10" style={ColorButtonComponent} aria-label="Search database" icon={<AddIcon />} />
-
-                </Flex>
+                    <Center>
+                        <Box ml="3">
+                            <Text textTransform="uppercase" fontWeight="black" fontSize="18">
+                                {name}
+                            </Text>
+                            <Text ml={{ base: 0, md: 3 }} fontSize="sm">{description}</Text>
+                            <Text ml={{ base: 0, md: 3 }}  fontSize="m" fontWeight="semibold">$ {price}</Text>
+                        </Box>
+                    </Center>
+                </Box>
             </Checkbox>
-            
+
         </AccordionPanel>
     );
 }
--- src/pages/Products/Components/Treatment/Treatment.js
+++ src/pages/Products/Components/Treatment/Treatment.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import { InfoOutlineIcon } from '@chakra-ui/icons'
+import { useHistory } from "react-router";
 
-import { AccordionItem, AccordionButton, Checkbox, AccordionIcon, Text } from '@chakra-ui/react'
+import { AccordionItem, AccordionButton, Checkbox, AccordionIcon, Text, Button, AccordionPanel, Divider, Center } from '@chakra-ui/react'
 import Item from '../Item/Item.js';
 
 const Treatment = ({ id, name, products }) => {
+    const history = useHistory();
     const prop = () => {
         const states = [];
         products.map(() => states.push(false))
@@ -35,15 +38,24 @@ const Treatment = ({ id, name, products }) => {
 
     return (
 
-        <AccordionItem bg="#EBFFF8" borderRadius="30" mb="30" pb="0.1">
+        <AccordionItem bg="#EBFFF8" borderRadius="30" mb="30" pb="0.1" >
             <h2>
-                <AccordionButton h="28" _expanded={{ pb: "5" }} >
+                <AccordionButton h="28" _expanded={{ pb: "5", bg: "blackAlpha.50", borderRadius: "30" }} _hover={{ bg: "blackAlpha.50", borderRadius: "30" }}>
                     <Checkbox size="lg" isChecked={allChecked} isIndeterminate={isIndeterminate} onChange={HandleChangeCheckAll}>
                         <Text color="#24903c" fontSize="20px" fontStyle="initial" textTransform="capitalize" fontWeight="bold">{name}</Text>
                     </Checkbox>
                     <AccordionIcon />
                 </AccordionButton>
             </h2>
+            <AccordionPanel >
+                <Center>
+                    <Button leftIcon={<InfoOutlineIcon />} colorScheme="pink" variant="solid" onClick={() => history.push('/product')}>
+                        Ver Instrucciones
+                    </Button>
+                </Center>
+                <Divider borderColor="green" mt="2" />
+            </AccordionPanel>
+
             {products.map((p, index) => <Item isChecked={checkedItems[index]} onChange={() => HandleChangeCheckOne(index)} id={p.id} name={p.name} url={p.url}
                 description={p.description} price={p.price} />)}
 
